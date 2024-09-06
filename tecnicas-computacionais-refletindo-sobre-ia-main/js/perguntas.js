c3a6f5b72b801d4b9826901e5a7e866fbefd3ce5
João Felipe Mildemberger Pereira <107053385+Kirby532@users.noreply.github.com>
2024-09-05 22:36:10 -0300

    Add files via upload

--- tecnicas-computacionais-refletindo-sobre-ia-main/js/perguntas.js
+++ tecnicas-computacionais-refletindo-sobre-ia-main/js/perguntas.js
@@ -1,9 +1,9 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "A guerra poderia sobre as ias ter sido evitada?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Não poderia ser realizada.",
                 afirmacao: [
                     "No início ficou com medo do que essa tecnologia pode fazer.",
                     "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
@@ -11,10 +11,10 @@ export const perguntas = [
                 proxima: 1,
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Poderia.",
                 afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                    "Sim, através de negociações diplomáticas mais eficazes, acordos de paz poderiam ter sido alcançados antes do início do conflito.",
+                    
                 ],
                 proxima: 2,
             },
